fe3e13196f1575c52516c99fe6216472defa129f
Omkar patil <omkarp666@gmail.com>
2022-10-15 18:57:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Card from  "./Card";
+
+import Sdata from "./Sdata";
+
+// console.log(Sdata.sname)
+
+// function ncard(val){cd
+//   // console.log(val)
+//   return(
+//     <Card 
+//       imgsrc={val.imgsrc}
+//       title={val.title}
+//       sname={val.sname}
+//       link={val.link}
+//     />
+//   );
+// }
+
+
+
+
+const App=()=>(
+<>
+<h1 className="heading">List of Top 5 Netflix Series</h1>
+
+{Sdata.map((val, index) => { //using arrow function
+  // console.log(index)  
+    return(
+      <Card 
+        key={val.id}
+        imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+      />
+    );
+})};
+</>
+);
 
 export default App;
---
+++ src/Sdata.js
@@ -0,0 +1,39 @@
+const Sdata = [
+{   
+    id: 1,
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BOTk2NzUyOTctZDdlMS00MDJlLTgzNTEtNzQzYjFhNjA0YjBjXkEyXkFqcGdeQXVyMjg1NDcxNDE@._V1_.jpg",
+    title:"A Netflix Orginal series",
+    sname:"DARK",
+    link:"https://www.netflix.com/in/title/80100172"
+},
+{   
+    id: 2,
+    imgsrc:"https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg",
+    title:"A Netflix Orginal series",
+    sname:"Stranger Things",
+    link:"https://www.netflix.com/in/title/80057281"
+},
+{
+    id: 3,
+    imgsrc:"https://i0.wp.com/feminisminindia.com/wp-content/uploads/2018/07/sacred-games-season-2-netflix.jpg?fit=1280%2C720&ssl=1.jpg",
+    title:"A Netflix Orginal series",
+    sname:"Sacred Games",
+    link:"https://www.netflix.com/in/title/80115328"
+},
+{
+    id: 4,
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BN2FiOWU4YzYtMzZiOS00MzcyLTlkOGEtOTgwZmEwMzAxMzA3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    title:"A Netflix Orginal series",
+    sname:"The Witcher",
+    link:"https://www.netflix.com/in/title/80189685"
+},
+{
+    id: 5,
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BNDJjMzc4NGYtZmFmNS00YWY3LThjMzQtYzJlNGFkZGRiOWI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    title:"A Netflix Orginal series",
+    sname:"Lucifer",
+    link:"https://www.netflix.com/in/title/80057918"
+}
+];
+
+export default Sdata;
