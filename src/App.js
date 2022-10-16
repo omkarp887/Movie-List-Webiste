681956e9b2f46033c72865bb74d0b82a989303ff
Omkar patil <omkarp666@gmail.com>
2022-10-16 14:55:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,41 +3,112 @@ import React from "react";
 import Card from  "./Card";
 
 import Sdata from "./Sdata";
+import { useState } from 'react'
 
-// console.log(Sdata.sname)
+// const [inputValue, search] = useState("");
 
-// function ncard(val){cd
-//   // console.log(val)
-//   return(
-//     <Card 
-//       imgsrc={val.imgsrc}
-//       title={val.title}
-//       sname={val.sname}
-//       link={val.link}
-//     />
-//   );
+// const onChangeHandler = event => {
+//    search(event.target.value);
+// };
+// var arr=[]
+// // console.log(Sdata.sname)
+
+// // function ncard(val){cd
+// //   // console.log(val)
+// //   return(
+// //     <Card 
+// //       imgsrc={val.imgsrc}
+// //       title={val.title}
+// //       sname={val.sname}
+// //       link={val.link}
+// //     />
+// //   );
+// // }
+
+// function setSearch(){
+//   arr = Sdata.filter((res)=>{
+//     return res.title.includes(val)
+//   })
+
 // }
 
 
 
+// const App=()=>(
+// <>
 
-const App=()=>(
-<>
-<h1 className="heading">List of Top 5 Netflix Series</h1>
 
-{Sdata.map((val, index) => { //using arrow function
-  // console.log(index)  
-    return(
-      <Card 
-        key={val.id}
-        imgsrc={val.imgsrc}
-        title={val.title}
-        sname={val.sname}
-        link={val.link}
-      />
-    );
-})};
-</>
-);
+// <input
+//    type="text"
+//    name="name"
+//    onChange={onChangeHandler}
+//    value={inputValue}
+// />
+// <h1 className="heading">List of Top Movies And Series</h1>
+
+// {(inputValue?arr:Sdata).map((val, index) => { //using arrow function
+//   // console.log(index)  
+//     return(
+//       <Card 
+//         key={val.id}
+//         imgsrc={val.imgsrc}
+//         title={val.title}
+//         sname={val.sname}
+//         link={val.link}
+//       />
+//     );
+// })};
+// </>
+// );
+
+
+
+const App=()=>{
+  const [inputValue, search] = useState("");
+  const [arr, setArr] = useState(Sdata);
+
+  const onChangeHandler = event => {
+     search(event.target.value);
+     let myArray = Sdata.filter((res)=>{
+      return res.sname.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setArr(myArray)
+    // console.log(arr);
+  };
+  
+
+  return(
+    <>
+
+
+    
+    <h1 className="heading">List of Top Movies And Series</h1>
+    <div className="custCenter">
+    <input
+      type="text"
+      name="name"
+      placeholder="search"
+      onChange={onChangeHandler}
+      value={inputValue}
+      className="searchbar"
+    />
+    </div>
+    {arr.map((val, index) => { //using arrow function
+      // console.log(index)  
+   
+        return(
+          
+          <Card 
+            key={val.id}
+            imgsrc={val.imgsrc}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+          />
+        );
+    })};
+    </>
+  )
+}
 
 export default App;
