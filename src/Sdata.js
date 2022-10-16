434bc7ddea5708a7870f56e014809a0a36daef7e
Omkar patil <omkarp666@gmail.com>
2022-10-16 13:16:08 +0530

    commit

--- src/Sdata.js
+++ src/Sdata.js
@@ -33,7 +33,43 @@ const Sdata = [
     title:"A Netflix Orginal series",
     sname:"Lucifer",
     link:"https://www.netflix.com/in/title/80057918"
+},
+{
+    id: 6,
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOg6r5nse-NhtKbqoJU1W7_2yxqPi9MLcbi78ZuQDP&s.jpg",
+    title:"20th Century Fox",
+    sname:"Avatar",
+    link:"https://www.amazon.com/Avatar-Sam-Worthington/dp/B003EVWDR0"
+},
+{
+    id: 7,
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UX32_CR0,0,32,44_AL_.jpg",
+    title:"20th Century Fox",
+    sname:"Titanic",
+    link:"https://www.hotstar.com/in/movies/titanic/1770001166"
+},
+{
+    id: 8,
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_QL75_UY148_CR0,0,100,148_.jpg",
+    title:"Walt Disney Studios Motion Pictures",
+    sname:"Star Wars: The Force Awakens",
+    link:"https://www.hotstar.com/in/movies/star-wars-the-rise-of-skywalker/1260025290"
+},
+{
+    id: 9,
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_QL75_UX100_CR0,0,100,148_.jpg",
+    title:"Walt Disney Studios Motion Pictures",
+    sname:"Avengers: Infinity War",
+    link:"https://www.hotstar.com/in/movies/avengers-infinity-war/1660010677"
+},
+{
+    id: 10,
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BNzQ3OTY4NjAtNzM5OS00N2ZhLWJlOWUtYzYwZjNmOWRiMzcyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX100_CR0,5,100,148_.jpg",
+    title:"Universal Pictures",
+    sname:"Jurassic World",
+    link:"https://www.primevideo.com/detail/amzn1.dv.gti.f2b36a92-d068-78f2-658e-b9e81ccdee72"
 }
 ];
 
 export default Sdata;
+
